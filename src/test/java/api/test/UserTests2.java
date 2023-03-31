ed85cb500ebf4bdff2bb1d15de5d913b74bda804
mayuresh-Potdar <mayureshpotdar13@gmail.com>
2023-03-31 13:06:55 +0530

    This is my very first commit

---
+++ src/test/java/api/test/UserTests2.java
@@ -0,0 +1,97 @@
+package api.test;
+
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+
+import api.endpoints.UserEndPoints2;
+import api.payload.User;
+import io.restassured.response.Response;
+
+public class UserTests2 {
+
+	Faker faker;
+	User userPayload;
+	public Logger logger;
+	
+	@BeforeClass
+	public void setup()
+	{
+		faker = new Faker();
+		userPayload = new User();
+		
+		userPayload.setId(faker.idNumber().hashCode());
+		userPayload.setUsername(faker.name().username());
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+		userPayload.setPassword(faker.internet().password(5,10));
+		userPayload.setPhone(faker.phoneNumber().cellPhone());
+		
+		// logs
+		logger=LogManager.getLogger(this.getClass());
+		logger.debug("debugging........");
+		
+	}
+	
+	@Test(priority=1)
+	public void testPostUser()
+	{
+		logger.info("***************** Creating User ********************");
+		Response response = UserEndPoints2.createUser(userPayload);
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(),200);
+		
+		logger.info("***************** User is created ********************");
+	}
+	
+	@Test(priority=2)
+	public void testGetUserByName()
+	{
+		logger.info("***************** Reading User info ********************");
+		Response response = UserEndPoints2.readUser(this.userPayload.getUsername());
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(),200);
+		
+		logger.info("***************** User info is displayed ********************");
+	}
+	
+	@Test(priority=3)
+	public void testUpdateUserByName()
+	{
+		
+		logger.info("***************** Updating user ********************");
+		// Update data using payload
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+		
+		Response response = UserEndPoints2.updateUser(this.userPayload.getUsername(),userPayload);
+		//response.then().log().body().statusCode(200);	// chai assertion 
+		response.then().log().all();
+		
+		Assert.assertEquals(response.getStatusCode(),200);	// testng assertion 
+		
+		logger.info("***************** User is updated ********************");
+		// Checking data after update
+		
+		Response responseAfterUpdate = UserEndPoints2.readUser(this.userPayload.getUsername());
+		Assert.assertEquals(responseAfterUpdate.getStatusCode(),200);
+		
+	}
+	
+	@Test(priority=4)
+	public void testDeleteUserByName()
+	{
+		logger.info("***************** Deleting User ********************");
+		Response response = UserEndPoints2.deleteUser(this.userPayload.getUsername());
+		Assert.assertEquals(response.getStatusCode(),200);
+		logger.info("***************** User Deleted ********************");
+	}
+}
---
+++ src/test/java/api/utilities/ExtentReportManager.java
@@ -0,0 +1,76 @@
+package api.utilities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+//Extent report 5.x
+
+public class ExtentReportManager implements ITestListener {
+
+	public ExtentSparkReporter sparkReporter;		// Responsible for UI of report
+	public ExtentReports extent;					// For projecting common data eg env info , user info, os info, project name , module name
+	public ExtentTest test;							// Responsible for creating entries in the report 
+	
+	String repName;
+	
+	public void onStart(ITestContext testContext)
+	{
+		String timeStamp = new  SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());	// time stamp
+		repName = "Test-Report-"+timeStamp+".html";
+		
+		sparkReporter = new ExtentSparkReporter(".\\Reports\\"+repName); 	// Specify location of the report
+		
+		sparkReporter.config().setDocumentTitle("RestAssuredAutomationProject");	// Title of the project
+		sparkReporter.config().setReportName("Pet Store Users API");	// name of the report
+		sparkReporter.config().setTheme(Theme.DARK);
+		
+		extent=new ExtentReports();
+		extent.attachReporter(sparkReporter);
+		extent.setSystemInfo("Application","Pet Store User API");
+		extent.setSystemInfo("Operating System",System.getProperty("os.name"));
+		extent.setSystemInfo("User Name",System.getProperty("user.name"));
+		extent.setSystemInfo("Environment","QA");
+		extent.setSystemInfo("user","Mayuresh");
+	}
+	
+	public void onTestSuccess(ITestResult result)
+	{
+		test=extent.createTest(result.getName());
+		test.assignCategory(result.getMethod().getGroups());
+		test.createNode(result.getName());
+		test.log(Status.PASS,"Test Passed");
+	}
+	
+	public void onTestFailure(ITestResult result)
+	{
+		test=extent.createTest(result.getName());
+		test.createNode(result.getName());
+		test.assignCategory(result.getMethod().getGroups());
+		test.log(Status.FAIL,"Test Failed");
+		test.log(Status.FAIL,result.getThrowable().getMessage());
+	}
+	
+	public void onTestSkipped(ITestResult result)
+	{
+		test=extent.createTest(result.getName());
+		test.createNode(result.getName());
+		test.assignCategory(result.getMethod().getGroups());
+		test.log(Status.SKIP,"Test Skipped");
+		test.log(Status.SKIP,result.getThrowable().getMessage());
+	}
+	
+	public void onFinish(ITestContext testContext)
+	{
+		extent.flush(); 	// if you forgot this then your report can not be generated
+	}
+}
